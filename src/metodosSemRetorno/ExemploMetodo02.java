11e0148ecfeed041db0c13d98a628e6b82526eb5
Vinicius <viniciusn11@hotmail.com>
2024-03-05 00:58:18 -0300

    Exemplo Comentado

---
+++ src/metodosSemRetorno/ExemploMetodo02.java
@@ -0,0 +1,10 @@
+package metodosSemRetorno;
+
+public class ExemploMetodo02 {
+	
+	// Criando o método s/ retorno e s/ parametro
+	public static void mensagem() {
+		System.out.println("Mensagem: Hello World");
+	}
+	
+}
